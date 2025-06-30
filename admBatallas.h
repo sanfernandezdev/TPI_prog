77ca37b1dffb092855be31f699aae1cc05673d51
Santiago <sanfernandezdev@gmail.com>
2025-06-30 09:39:06 -0300

    Se agregaron comentarios

--- admBatallas.h
+++ admBatallas.h
@@ -8,5 +8,9 @@ bool evaluarHabActiva(const std::vector<float>& recursosJugador);
 float sumarChancesSegunSoldados(int tropas);
 bool seGanoRonda(float chance_hab);
 void ejecutarPasiva(std::vector<float>& recursosJugador, int rondaActual, int casaElegida, int& tropasCaidas, int& tropasListas, int& tropasAdicionales, int estadisticas[]);
+int getComidaGastadaRonda(int cantSoldados, std::vector<float>& recursosJugador, int casaElegida);
+int getTropasListas(const std::vector<float> recursosJugador);
+bool seActivoChanceHab(float chance_hab);
+void ejecutarActiva(std::vector<float>& recursosJugador, bool& rondaGanada, int& oroAGanar, int& tropasListas);
 
 #endif // BATALLAS_H_INCLUDED
